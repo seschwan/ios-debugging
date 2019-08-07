ec672e9510c1eb43943cb706603f1c1123547c42
Seschwan <seschwan@Seschwans-MacBook-Pro.local>
2019-08-06 22:48:21 -0600

    Fixed Transition Bug

--- JournalCoreData/View Controllers/EntriesTableViewController.swift
+++ JournalCoreData/View Controllers/EntriesTableViewController.swift
@@ -110,7 +110,7 @@ class EntriesTableViewController: UITableViewController, NSFetchedResultsControl
         case "ViewEntry":
             guard let destinationVC = segue.destination as? EntryDetailViewController,
                 let indexPath = tableView.indexPathForSelectedRow else { return }
-            
+            destinationVC.entryController = entryController
             destinationVC.entry = fetchedResultsController.object(at: indexPath)
             
         default:
--- JournalCoreData/View Controllers/EntryDetailViewController.swift
+++ JournalCoreData/View Controllers/EntryDetailViewController.swift
@@ -47,8 +47,8 @@ class EntryDetailViewController: UIViewController {
         }
         
         title = entry.title
-        titleTextField.text = entry.title
-        bodyTextView.text = entry.bodyText
+        titleTextField?.text = entry.title
+        bodyTextView?.text = entry.bodyText
         
         var segmentIndex = 0
         
@@ -63,7 +63,7 @@ class EntryDetailViewController: UIViewController {
             break
         }
         
-        moodSegmentedControl.selectedSegmentIndex = segmentIndex
+        moodSegmentedControl?.selectedSegmentIndex = segmentIndex
     }
     
     var entry: Entry? {
